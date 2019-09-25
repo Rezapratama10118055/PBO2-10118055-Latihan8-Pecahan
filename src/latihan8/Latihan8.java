385baedafe795ff962843a8ed108db201a684997
User <User@DESKTOP-2RC2UP5>
2019-09-25 08:48:02 +0700

    first commit

---
+++ src/latihan8/Latihan8.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan8;
+
+/**
+ *
+ * @author User
+ * NAMA:REZA PRATAMA
+ * KELAS:PBO2
+ * NIM:10118055
+ * Deskripsi:program ini berisi program untuk menampilkan
+ * jenis jenis data bilangan bulat
+ */
+public class Latihan8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f= 1234567890.123456789F;
+        d=1_234_567_890.123456789;
+        System.out.println("float:"+f);
+        System.out.println("double:"+d);
+    }
+    
+}
